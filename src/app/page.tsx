6a4da386ed70064ab213f0516c6ff8f3cb2bd405
XRPL.to <93736363+xrplto@users.noreply.github.com>
2024-12-08 05:03:49 -0500

    fixed building error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -420,7 +420,7 @@ export default function Home() {
                   className="win98-button h-[18px] w-[18px] flex items-center justify-center p-0"
                   onClick={() => setIsHelpOpen(false)}
                 >
-                  ��
+                  ×
                 </button>
               </div>
             </div>
